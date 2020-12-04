ad5cad85adf0bb384f7b1c33a5165c016cc75728
Daniel Vega <danielalexvega@gmail.com>
2020-12-04 11:58:23 -0600

    work on success page and newsletter/netlify form

--- src/pages/newsletter.js
+++ src/pages/newsletter.js
@@ -1,7 +1,24 @@
 import React from 'react'
 import Layout from '../components/Layout'
 const NewsLetter = () => {
-  return <h2>newsletter page</h2>
+  return (
+    <Layout>
+      <section className="newsletter-page">
+        <div className="page-center">
+          <h2>Get all the latest stories to your inbox.</h2>
+          <h4>I write to my digital friends every few weeks.</h4>
+          <form action="/success" className="contact-form" name="contact"
+            method="POST" netlify-honeypot="bot-field" data-netlify="true">
+            <input type="hidden" name="bot-field" />
+            <input type="hidden" name="contact" />
+            <input type="text" name="name" placeholder="Your name" className="form-control" />
+            <input type="email" name="email" placeholder="Your email" className="form-control" />
+            <button className="btn form-control submit-btn" type="submit">Subscribe</button>
+          </form>
+        </div>
+      </section>
+    </Layout>
+  )
 }
 
 export default NewsLetter
