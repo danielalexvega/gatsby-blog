9954c63c5ea7b33770ff9c56b872d92ee663d442
Daniel Vega <danielalexvega@gmail.com>
2020-12-08 21:57:00 -0600

    work on posts page

--- src/pages/posts.js
+++ src/pages/posts.js
@@ -1,16 +1,44 @@
-import React from 'react'
-import Layout from '../components/Layout'
-import Hero from '../components/Hero'
-import { graphql } from 'gatsby'
-import Posts from '../components/Posts'
-// ...GatsbyImageSharpFluid
+import React from 'react';
+import Layout from '../components/Layout';
+import Hero from '../components/Hero';
+import { graphql } from 'gatsby';
+import Posts from '../components/Posts';
+
 const PostsPage = ({ data }) => {
+  const { allMdx: { nodes: posts } } = data;
+
   return (
     <Layout>
       <Hero />
-
+      <Posts posts={posts} title="all posts" />
     </Layout>
   )
 }
 
-export default PostsPage
+export const query = graphql`
+  {
+    allMdx(sort: {fields: frontmatter___date, order: DESC}) {
+      nodes {
+        excerpt
+        frontmatter {
+          author
+          category
+          slug
+          title
+          date(formatString: "MMMM Do, YYYY")
+          readTime
+          image {
+            childImageSharp {
+              fluid {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+        }
+        id
+      }
+    }
+  }
+`
+
+export default PostsPage;
