ac58737334c4eb8063f1b0f1751758199f903688
Daniel Vega <danielalexvega@gmail.com>
2020-12-11 18:06:51 -0600

    work on gatsby-ssr and gatsby-browser to set up MDX

---
+++ root-mdx.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { MDXProvider } from '@mdx-js/react';
+
+const components = {
+  //all the logic
+
+}
+
+export const wrapMDX = ({ element }) => {
+  return <MDXProvider components={components}>
+    {element}
+  </MDXProvider>
+}
