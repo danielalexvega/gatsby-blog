5234a9b677072917d90abd66e257f6c577d1cfac
Daniel Vega <danielalexvega@gmail.com>
2020-12-13 22:19:04 -0600

    work with root-mdx and write custom components

--- root-mdx.js
+++ root-mdx.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import { MDXProvider } from '@mdx-js/react';
 
-const components = {
 
+const myH2 = props => {
+  if (props.title) {
+    return <h2 {...props} style={{ fontSize: "2rem", color: "red" }}>{props.children}</h2>
+  }
+  return <h2 {...props} className='code'>{props.children}</h2>
+}
+
+
+
+// You can rewrite html elements. Or, you can define special html elements in a sense... 
+// You are the "king of the castle"
+
+const components = {
+  h2: myH2
 
 }
 
